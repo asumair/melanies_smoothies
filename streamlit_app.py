f1d64faf45f15688cb5cd27d2fa59bf37ce4b959
Mohammed Sumair <asumair@gmail.com>
2026-02-05 13:59:10 +0530

    Restore watermelon API call and dataframe display
    Reintroduced the API call to fetch watermelon data and display it in the app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,9 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
+
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothies! :cup_with_straw:")
@@ -37,10 +35,13 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        #st.text(smoothiefroot_response.json())
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
     #st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+    #my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string+"""','""" + name_on_order +"""')"""
 
     #st.write(my_insert_stmt)
